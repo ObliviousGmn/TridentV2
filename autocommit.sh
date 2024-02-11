16b5d56b01c20438e834c4bff6e707b48c0c008b
ObliviousGmn <german.thomas00@gmail.com>
2024-02-11 12:55:58 -0600

    Feb 11 2024

--- autocommit.sh
+++ autocommit.sh
@@ -75,8 +75,7 @@ push_config(){
   cd $config_folder
   git pull origin $branch --no-rebase
   git add .
-  current_date=$(date +"%Y-%m-%d %T")
-  git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
+  git commit -m "`date +'%b %d %Y'`"
   git push origin $branch
 }
 
